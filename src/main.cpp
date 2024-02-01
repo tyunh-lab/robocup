691212a0e0bd1eac75b68ad029da4a245151dc4c
tyunh-lab <tyunhs@gmail.com>
2024-02-01 15:20:07 +0900

    コードがごちゃごちゃだったから綺麗にした

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,125 @@
 #include <Arduino.h>
+#include "jyro.h"
+#include "ble.cpp"
+#include "cam.cpp"
 
+// モーター設定
 int pwm[] = {12, 14, 26, 27};
 
-void setup()
-{
-  Serial.begin(115200);
-  for (int i = 0; i < 4; i++)
-  {
-    pinMode(pwm[i], OUTPUT);
-  }
-}
-
-//173 - 128 = 45
-//128 - 45 = 83
+double angle;
 
 void forword()
 {
-  analogWrite(pwm[0], 83);
-  analogWrite(pwm[1], 83);
-  analogWrite(pwm[2], 173);
-  analogWrite(pwm[3], 173);
+  analogWrite(pwm[0], 10);
+  analogWrite(pwm[1], 236);
+  analogWrite(pwm[2], 236);
+  analogWrite(pwm[3], 10);
 }
 
-void backword()
+void back()
 {
-  digitalWrite(pwm[0], LOW);
-  digitalWrite(pwm[1], HIGH);
-  digitalWrite(pwm[2], LOW);
-  digitalWrite(pwm[3], HIGH);
+  analogWrite(pwm[0], 236);
+  analogWrite(pwm[1], 10);
+  analogWrite(pwm[2], 10);
+  analogWrite(pwm[3], 236);
 }
 
 void left()
 {
-  digitalWrite(pwm[0], LOW);
-  digitalWrite(pwm[1], HIGH);
-  digitalWrite(pwm[2], HIGH);
-  digitalWrite(pwm[3], LOW);
+  analogWrite(pwm[0], 226);
+  analogWrite(pwm[1], 226);
+  analogWrite(pwm[2], 226);
+  analogWrite(pwm[3], 226);
 }
 
 void right()
 {
-  digitalWrite(pwm[0], HIGH);
-  digitalWrite(pwm[1], LOW);
-  digitalWrite(pwm[2], LOW);
-  digitalWrite(pwm[3], HIGH);
+  analogWrite(pwm[0], 10);
+  analogWrite(pwm[1], 10);
+  analogWrite(pwm[2], 10);
+  analogWrite(pwm[3], 10);
 }
+
+void setup()
+{
+  Serial.begin(115200);
+
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(pwm[i], OUTPUT);
+  }
+
+  // Lちか
+  pinMode(2, OUTPUT);
+  initBLE();
+  iniCAM();
+
+  // Serial.setDebugOutput(true);
+  // Serial.println();
+
+  Serial.print("jyro setupping....");
+  Wire.setPins(21, 22);
+  jy901.StartIIC();
+  Serial.println("done");
+}
+
+// 　TODO: Setup関数内で初期値を設定させるので、電源投下時を0として計算させる。
+// 　      もしくは、回路を変更させずに絶対値にするのもあり。
+
+double errorRange = 2; // 2度の誤差を許容する
 
 void loop()
 {
-  forword();
-  delay(1000);
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+
+  // このブロックはisolateさせて、真っ直ぐにさせる精度を上げるかも
+  jy901.GetAngle();
+  angle = (float)jy901.stcAngle.Angle[2] / 32768 * 180;
+  /*ここからいけるかわからん*/
+  pCharacteristic->setValue(angle);
+  pCharacteristic->notify();
+  /*ここまで*/
+  Serial.print("angle:");
+  Serial.println(angle);
+
+  if (angle > errorRange)
+  {
+    while (angle > errorRange)
+    {
+      right();
+    }
+
+    Serial.println("right");
+  }
+  else if (angle < errorRange * -1)
+  {
+    while (angle < errorRange * -1)
+    {
+      left();
+    }
+    Serial.println("left");
+  }
+  else
+  {
+    forword();
+    Serial.println("forword");
+  }
+
+  delay(400);
+  digitalWrite(2, HIGH);
+  delay(400);
+  digitalWrite(2, LOW);
 }
