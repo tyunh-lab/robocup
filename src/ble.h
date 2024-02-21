6c7cdea172e0d0059b5df15a8c2c350234cc5ca2
tyunh-lab <tyunhs@gmail.com>
2024-02-21 14:28:26 +0900

    ラインセンサーを読んでコートに出ないように変更した

---
+++ src/ble.h
@@ -0,0 +1,79 @@
+// #include <Arduino.h>
+// #include <BLEDevice.h>
+// #include <BLEServer.h>
+// #include <BLEUtils.h>
+// #include <BLE2902.h>
+
+// // BLE設定
+// BLEServer *pServer = NULL;
+// BLECharacteristic *pCharacteristic = NULL;
+// bool deviceConnected = false;
+// bool oldDeviceConnected = false;
+
+// #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+// #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+// class MyServerCallbacks : public BLEServerCallbacks
+// {
+//     void onConnect(BLEServer *pServer)
+//     {
+//         deviceConnected = true;
+//         BLEDevice::startAdvertising();
+//     };
+
+//     void onDisconnect(BLEServer *pServer)
+//     {
+//         deviceConnected = false;
+//     }
+// };
+
+// int getA;
+
+// class MyCallbacks : public BLECharacteristicCallbacks
+// {
+//     void onWrite(BLECharacteristic *pCharacteristic)
+//     {
+//         std::string value = pCharacteristic->getValue();
+//         if (value.length() > 0)
+//         {
+//             for (int i = 0; i < value.length(); i++)
+//             {
+//                 Serial.print(value[i]);
+//                 getA += value[i];
+//             }
+//             if (getA > 300 || getA < 100)
+//             {
+//                 // forword();
+//             }
+//             getA = 0;
+//             Serial.println();
+//         }
+//     }
+// };
+
+// void initBLE()
+// {
+//     // Create the BLE Device
+//     BLEDevice::init("ESP32");
+//     // Create the BLE Server
+//     pServer = BLEDevice::createServer();
+//     pServer->setCallbacks(new MyServerCallbacks());
+
+//     // Create the BLE Service
+//     BLEService *pService = pServer->createService(SERVICE_UUID);
+
+//     // Create a BLE Characteristic
+//     pCharacteristic = pService->createCharacteristic(
+//         CHARACTERISTIC_UUID,
+//         BLECharacteristic::PROPERTY_READ |
+//             BLECharacteristic::PROPERTY_WRITE |
+//             BLECharacteristic::PROPERTY_NOTIFY |
+//             BLECharacteristic::PROPERTY_INDICATE);
+
+//     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+//     pAdvertising->addServiceUUID(SERVICE_UUID);
+//     pAdvertising->setScanResponse(false);
+//     pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
+//     BLEDevice::startAdvertising();
+//     Serial.println("Waiting a client connection to notify...");
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,151 +1,360 @@
+// #include <Arduino.h>
+// #include "jyro.h"
+// // #include "ble.cpp"
+// #include "cam.h"
+
+// // モーター設定
+// int pwm[] = {12, 14, 26, 27};
+
+// double angle;
+
+// void setup()
+// {
+//   Serial.begin(115200);
+
+//   // モーター初期化
+//   for (int i = 0; i < 4; i++)
+//   {
+//     pinMode(pwm[i], OUTPUT);
+//     ledcSetup(i, 169000, 8); // 100kHz, 8bit(256段階)
+//     ledcAttachPin(pwm[i], i);
+//   }
+
+//   // Lちか
+//   pinMode(2, OUTPUT);
+//   // initBLE();
+//   initCAM();
+
+//   // Serial.setDebugOutput(true);
+//   // Serial.println();
+
+//   Serial.print("jyro setupping....");
+//   Wire.setPins(21, 22);
+//   jy901.StartIIC();
+//   Serial.println("done");
+// }
+
+// void stop()
+// {
+//   ledcWrite(0, 128);
+//   ledcWrite(1, 120);
+//   ledcWrite(2, 130);
+//   ledcWrite(3, 118);
+// }
+
+// void foward()
+// {
+//   ledcWrite(0, 10);
+//   ledcWrite(1, 200);
+//   ledcWrite(2, 10);
+//   ledcWrite(3, 200);
+// }
+
+// void back()
+// {
+//   ledcWrite(0, 200);
+//   ledcWrite(1, 10);
+//   ledcWrite(2, 200);
+//   ledcWrite(3, 10);
+// }
+
+// void left()
+// {
+//   ledcWrite(0, 10);
+//   ledcWrite(1, 10);
+//   ledcWrite(2, 200);
+//   ledcWrite(3, 200);
+// }
+
+// void right()
+// {
+//   ledcWrite(0, 200);
+//   ledcWrite(1, 200);
+//   ledcWrite(2, 10);
+//   ledcWrite(3, 10);
+// }
+
+// /*
+// explain:
+// モーターの周波数を設定する関数
+// usage:
+//   motor: motor pin 0-3 順に[12,14,26,27]
+//   frequency: 周波数 0-255
+//   useAutoConversion (optional): モーターの位置を考慮して、自動的に255を引くかどうか（デフォルトはfalse）
+// */
+// void set_frequency_for_uart(int motor, int frequency, bool useAutoConversion = false)
+// {
+//   if (!useAutoConversion)
+//   {
+//     analogWrite(motor, frequency);
+//   }
+//   else
+//   {
+//     if (motor == 0 || motor == 3)
+//     {
+//       analogWrite(motor, 255 - frequency);
+//     }
+//     else
+//     {
+//       ledcWrite(motor, frequency);
+//     }
+//   }
+// }
+
+// // 　TODO: Setup関数内で初期値を設定させるので、電源投下時を0として計算させる。
+// // 　      もしくは、回路を変更させずに絶対値にするのもあり。
+// double errorRange = 2; // 2度の誤差を許容する
+
+// void loop()
+// {
+//   stop();
+//   delay(200);
+//   foward();
+//   delay(1000);
+//   stop();
+//   delay(200);
+//   right();
+//   delay(1000);
+//   stop();
+//   delay(200);
+//   left();
+//   delay(1000);
+//   stop();
+//   delay(200);
+//   back();
+//   delay(1000);
+
+//   // // disconnecting
+//   // if (!deviceConnected && oldDeviceConnected)
+//   // {
+//   //   delay(500);                  // give the bluetooth stack the chance to get things ready
+//   //   pServer->startAdvertising(); // restart advertising
+//   //   Serial.println("start advertising");
+//   //   oldDeviceConnected = deviceConnected;
+//   // }
+//   // // connecting
+//   // if (deviceConnected && !oldDeviceConnected)
+//   // {
+//   //   // do stuff here on connecting
+//   //   oldDeviceConnected = deviceConnected;
+//   // }
+
+//   // // このブロックはisolateさせて、真っ直ぐにさせる精度を上げるかも
+//   // jy901.GetAngle();
+//   // angle = (float)jy901.stcAngle.Angle[2] / 32768 * 180;
+//   // /*ここからいけるかわからん*/
+//   // pCharacteristic->setValue(angle);
+//   // pCharacteristic->notify();
+//   // /*ここまで*/
+//   // Serial.print("angle:");
+//   // Serial.println(angle);
+
+//   // if (angle > errorRange)
+//   // {
+//   //   while (angle > errorRange)
+//   //   {
+//   //     right();
+//   //   }
+
+//   //   Serial.println("right");
+//   // }
+//   // else if (angle < errorRange * -1)
+//   // {
+//   //   while (angle < errorRange * -1)
+//   //   {
+//   //     left();
+//   //   }
+//   //   Serial.println("left");
+//   // }
+//   // else
+//   // {
+//   //   forword();
+//   //   Serial.println("forword");
+//   // }
+
+//   // delay(400);
+//   // digitalWrite(2, HIGH);
+//   // delay(400);
+//   // digitalWrite(2, LOW);
+// }
+
 #include <Arduino.h>
-#include "jyro.h"
-#include "ble.cpp"
-#include "cam.cpp"
+
+TaskHandle_t thp[1];
 
 // モーター設定
-int pwm[] = {12, 14, 26, 27};
+int motorPin[] = {12, 14, 26, 27};
 
-double angle;
+#define HIGH 200
+#define LOW 55
 
-void forword()
+// 機体の動作方向(0:停止, 1:前進, 2:後退, 3:左, 4:右)
+// int stats = 0;
+
+// モーター制御権限(重複実行防止)
+// int privilegeCore = 0;
+int privilegeCore = 1;
+
+// 　モーター指示
+int order = 0;
+
+void stop(int core)
 {
-  analogWrite(pwm[0], 10);
-  analogWrite(pwm[1], 236);
-  analogWrite(pwm[2], 236);
-  analogWrite(pwm[3], 10);
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, 128);
+  ledcWrite(1, 120);
+  ledcWrite(2, 130);
+  ledcWrite(3, 118);
+}
+void foward(int core)
+{
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, LOW);
+  ledcWrite(1, HIGH);
+  ledcWrite(2, HIGH);
+  ledcWrite(3, LOW);
+}
+void back(int core)
+{
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, HIGH);
+  ledcWrite(1, LOW);
+  ledcWrite(2, LOW);
+  ledcWrite(3, HIGH);
+}
+void left(int core)
+{
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, LOW);
+  ledcWrite(1, LOW);
+  ledcWrite(2, HIGH);
+  ledcWrite(3, HIGH);
+}
+void right(int core)
+{
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, HIGH);
+  ledcWrite(1, HIGH);
+  ledcWrite(2, LOW);
+  ledcWrite(3, LOW);
+}
+void turnl(int core)
+{
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, HIGH);
+  ledcWrite(1, HIGH);
+  ledcWrite(2, HIGH);
+  ledcWrite(3, HIGH);
+}
+void turnr(int core)
+{
+  if (privilegeCore != core)
+    return;
+  ledcWrite(0, LOW);
+  ledcWrite(1, LOW);
+  ledcWrite(2, LOW);
+  ledcWrite(3, LOW);
 }
 
-void back()
+void loop()
 {
-  analogWrite(pwm[0], 236);
-  analogWrite(pwm[1], 10);
-  analogWrite(pwm[2], 10);
-  analogWrite(pwm[3], 236);
+  // メインループ (core 0)
+  // Serial.println("line sensor value: " + String(analogRead(15)));
+
+  delay(50);
+  if (analogRead(15) >= 20)
+  {
+    Serial.println(String(order));
+    switch (order)
+    {
+    case 0:
+      // privilegeCore = 0;
+      order = 1;
+      // privilegeCore = 1;
+      break;
+
+    case 1:
+      // privilegeCore = 0;
+      order = 0;
+      // privilegeCore = 1;
+      break;
+
+    case 2:
+      // privilegeCore = 0;
+      order = 3;
+      // privilegeCore = 1;
+      break;
+
+    case 3:
+      // privilegeCore = 0;
+      order = 2;
+      // privilegeCore = 1;
+      break;
+
+    default:
+      Serial.println("something wrong!");
+      order = 1;
+      break;
+    }
+  }
+  else
+  {
+    order - 1;
+  }
 }
 
-void left()
+// for Core0a
+void doStop()
 {
-  analogWrite(pwm[0], 226);
-  analogWrite(pwm[1], 226);
-  analogWrite(pwm[2], 226);
-  analogWrite(pwm[3], 226);
+  stop(1);
+  delay(200);
 }
 
-void right()
+void Core0a(void *args)
 {
-  analogWrite(pwm[0], 10);
-  analogWrite(pwm[1], 10);
-  analogWrite(pwm[2], 10);
-  analogWrite(pwm[3], 10);
+  while (1)
+  {
+    // サブで実行するプログラム (core 1)
+    if (order == 0)
+    {
+      foward(1);
+    }
+    else if (order == 1)
+    {
+      back(1);
+    }
+    else if (order == 2)
+    {
+      left(1);
+    }
+    else if (order == 3)
+    {
+      right(1);
+    }
+    else
+    {
+      // ボールに向かわせる
+      foward(1);
+      order = 0;
+    }
+    delay(1);
+  }
 }
 
 void setup()
 {
-  Serial.begin(115200);
-
   for (int i = 0; i < 4; i++)
   {
-    pinMode(pwm[i], OUTPUT);
+    pinMode(motorPin[i], OUTPUT);
+    ledcSetup(i, 169000, 8); // 100kHz, 8bit(256段階)
+    ledcAttachPin(motorPin[i], i);
   }
-
-  // Lちか
-  pinMode(2, OUTPUT);
-  initBLE();
-  initCAM();
-
-  // Serial.setDebugOutput(true);
-  // Serial.println();
-
-  Serial.print("jyro setupping....");
-  Wire.setPins(21, 22);
-  jy901.StartIIC();
-  Serial.println("done");
-}
-
-/*
-explain:
-モーターの周波数を設定する関数
-usage:
-  motor: motor pin 0-3 順に[12,14,26,27]
-  frequency: 周波数 0-255
-  useAutoConversion (optional): モーターの位置を考慮して、自動的に255を引くかどうか（デフォルトはfalse）
-*/
-void set_frequency_for_uart(int motor, int frequency, bool useAutoConversion = false)
-{
-  if (!useAutoConversion)
-  {
-    analogWrite(pwm[motor], frequency);
-  }
-  else
-  {
-    if (motor == 0 || motor == 3)
-    {
-      analogWrite(pwm[motor], 255 - frequency);
-    }
-    else
-    {
-      analogWrite(pwm[motor], frequency);
-    }
-  }
-}
-
-// 　TODO: Setup関数内で初期値を設定させるので、電源投下時を0として計算させる。
-// 　      もしくは、回路を変更させずに絶対値にするのもあり。
-double errorRange = 2; // 2度の誤差を許容する
-
-void loop()
-{
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected)
-  {
-    delay(500);                  // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising(); // restart advertising
-    Serial.println("start advertising");
-    oldDeviceConnected = deviceConnected;
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected)
-  {
-    // do stuff here on connecting
-    oldDeviceConnected = deviceConnected;
-  }
-
-  // このブロックはisolateさせて、真っ直ぐにさせる精度を上げるかも
-  jy901.GetAngle();
-  angle = (float)jy901.stcAngle.Angle[2] / 32768 * 180;
-  /*ここからいけるかわからん*/
-  pCharacteristic->setValue(angle);
-  pCharacteristic->notify();
-  /*ここまで*/
-  Serial.print("angle:");
-  Serial.println(angle);
-
-  if (angle > errorRange)
-  {
-    while (angle > errorRange)
-    {
-      right();
-    }
-
-    Serial.println("right");
-  }
-  else if (angle < errorRange * -1)
-  {
-    while (angle < errorRange * -1)
-    {
-      left();
-    }
-    Serial.println("left");
-  }
-  else
-  {
-    forword();
-    Serial.println("forword");
-  }
-
-  // delay(400);
-  // digitalWrite(2, HIGH);
-  // delay(400);
-  // digitalWrite(2, LOW);
+  Serial.begin(115200);
+  xTaskCreatePinnedToCore(Core0a, "Core0a", 4096, NULL, 3, &thp[0], 0);
 }
