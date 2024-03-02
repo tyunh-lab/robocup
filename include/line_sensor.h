780239093f60c3f9f4e87bb81eccc8837b885c94
四津谷　舜 <tyunhs@gmail.com>
2024-03-03 02:27:27 +0900

    効率化

--- include/line_sensor.h
+++ include/line_sensor.h
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <pins.h>
 
+int line_sensor_pins[4] = {FRONT_LINE_SENSOR_PIN, LEFT_LINE_SENSOR_PIN, RIGHT_LINE_SENSOR_PIN, BACK_LINE_SENSOR_PIN};
+
 void setupLineSensor()
 {
     pinMode(FRONT_LINE_SENSOR_PIN, INPUT);
@@ -9,7 +11,15 @@ void setupLineSensor()
     pinMode(BACK_LINE_SENSOR_PIN, INPUT);
 }
 
-int readLineSensor(int pin)
+/*
+index 0: front
+index 1: left
+index 2: right
+index 3: back
+
+This function return the line sensors data.
+*/
+int readLineSensor(int index)
 {
-    return analogRead(pin);
+    return analogRead(line_sensor_pins[index]);
 }
