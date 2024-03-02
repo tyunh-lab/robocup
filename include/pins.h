f54e0368c8c2c63610d404c5319d87f9228a4865
四津谷　舜 <tyunhs@gmail.com>
2024-03-02 17:01:44 +0900

    pinの定義

---
+++ include/pins.h
@@ -0,0 +1,17 @@
+// uart pins
+#define UART_RX_PIN PC11
+#define UART_TX_PIN PC10
+// line sensor pins
+#define FRONT_LINE_SENSOR_PIN A0
+#define LEFT_LINE_SENSOR_PIN A1
+#define RIGHT_LINE_SENSOR_PIN A2
+#define BACK_LINE_SENSOR_PIN A3
+// ultrasonic sensor pins
+#define FRONT_TRIG D3
+#define FRONT_ECHO D2
+#define LEFT_TRIG D5
+#define LEFT_ECHO D4
+#define RIGHT_TRIG D7
+#define RIGHT_ECHO D6
+#define BACK_TRIG D9
+#define BACK_ECHO D8
