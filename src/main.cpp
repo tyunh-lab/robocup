53bc9c3e87f8c7951440bf7b12d477852acbb7a5
四津谷　舜 <tyunhs@gmail.com>
2024-03-03 02:28:07 +0900

    uart対応

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,14 @@ HardwareSerial Serial3(UART_RX_PIN, UART_TX_PIN);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
   delay(2000);
   Serial.print("Starting setup...");
   setupLineSensor();
   Serial.println("done");
   Serial.print("Starting UART...");
-  Serial3.begin(9600);
+  Serial3.begin(115200);
   Serial.println("done");
   Serial.print("Setup ultrasonic sensor...");
   setupUltrasonicSensor();
@@ -31,10 +31,34 @@ void setup()
   }
 }
 
+double front_distance = 0;
+double left_distance = 0;
+double right_distance = 0;
+double back_distance = 0;
+
+int front_line_sensor = 0;
+int left_line_sensor = 0;
+int right_line_sensor = 0;
+int back_line_sensor = 0;
+
 void loop()
 {
-  int front = readUltrasonicSensor(0);
-  Serial.print("Front: ");
-  Serial.println(front);
-  delay(200);
+  // front_distance = readUltrasonicSensor(0);
+  // left_distance = readUltrasonicSensor(1);
+  // right_distance = readUltrasonicSensor(2);
+  // back_distance = readUltrasonicSensor(3);
+
+  front_line_sensor = readLineSensor(0);
+  left_line_sensor = readLineSensor(1);
+  right_line_sensor = readLineSensor(2);
+  back_line_sensor = readLineSensor(3);
+
+  // String i = Serial3.readString();
+  // Serial.println(i);
+  if (Serial3.available())
+  {
+    String i = Serial3.readString();
+    Serial.println(i);
+    Serial3.println(String(front_line_sensor) + " " + String(left_line_sensor) + " " + String(right_line_sensor) + " " + String(back_line_sensor));
+  }
 }
