98733d995f51f2bf6ba50dbe974aea2a13a44e53
四津谷　舜 <tyunhs@gmail.com>
2024-03-08 16:34:36 +0900

    ドリブラーの制御変更

--- include/dribble.h
+++ include/dribble.h
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 #include <Servo.h>
 #include <pins.h>
 
@@ -7,17 +7,23 @@ Servo esc; // Servoオブジェクトを作成
 void initDribbler()
 {
     esc.attach(DRIBBLER_MOTOR_PIN); // サーボモータをDribblerピンに接続
+    esc.writeMicroseconds(1000);    // サーボモータを初期位置に
 }
 
 void dribble()
 {
-    // 停止
-    esc.writeMicroseconds(1000);
-    delay(2000);
+    // // 停止
+    // esc.writeMicroseconds(1000);
+    // delay(2000);
     // ハーフスロットル
     esc.writeMicroseconds(1500);
-    delay(2000);
-    // フルスロットル
-    esc.writeMicroseconds(2000);
-    delay(2000);
+    // delay(2000);
+    // // フルスロットル
+    // esc.writeMicroseconds(2000);
+    // delay(2000);
+}
+
+void stop_dribble()
+{
+    esc.writeMicroseconds(1000);
 }
