d713941f6a40ee4734c9e8e49377c554307c4537
四津谷　舜 <tyunhs@gmail.com>
2024-03-10 16:52:44 +0900

    システム開始音の別ファイル化

---
+++ src/mero.h
@@ -0,0 +1,28 @@
+int melody[] = {
+    165,
+    220,
+    330,
+    494,
+    659,
+    880,
+    1661,
+};
+
+int noteDurations[] = {
+    6, 6, 6, 6, 6, 6, 2};
+
+void playMelody(int buzzerPin)
+{
+    for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++)
+    {
+        int duration = 1000 / noteDurations[i];
+        tone(buzzerPin, melody[i], duration);
+
+        // 周波数と長さの後に一定の遅延
+        int pauseBetweenNotes = duration * 1.30;
+        delay(pauseBetweenNotes);
+
+        // ブザーを停止
+        noTone(buzzerPin);
+    }
+}
